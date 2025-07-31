ad158853e43375f1fdf4c611756373b8c5586224
Ayush007-pro <ayush82194@gmail.com>
2025-08-01 03:23:05 +0530

    updated metaData and created complete dir map to ease developement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+// import Navbar from "../components/navBar_v1";
 
 export default function Home() {
   return (
     <div className="font-unsaid grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      {/* <Navbar /> */}
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className=""
           src="/next.svg"
           alt="Next.js logo"
           width={180}
