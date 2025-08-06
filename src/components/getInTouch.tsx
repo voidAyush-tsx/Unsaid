2bf3a6e67e99fef503e8d045a84549b8601230eb
Ayush007-pro <ayush82194@gmail.com>
2025-08-06 13:59:16 +0530

    motions added to feature section

--- src/components/getInTouch.tsx
+++ src/components/getInTouch.tsx
@@ -6,7 +6,6 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 const getInTouch: React.FC = () => {
   const { scrollYProgress } = useScroll();
   const opacity = useTransform(scrollYProgress, [0.575, 0.65], [0, 1]);
-  // const y = useTransform(scrollYProgress, [0.6, 0.675], [200, 0]);
   const scale = useTransform(scrollYProgress, [0.6, 0.675], [0.5, 1]);
   
   return (
